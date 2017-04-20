2394adc25bfb46d62f2c401f254c1616c677e54a
Neo Chen <Neo Chen netkiller@msn.com>
2017-04-20 22:59:39 +0800

    Struts2 firewall

--- README.md
+++ README.md
@@ -96,3 +96,13 @@ Testing API
     single.show()
     #single.run()
     #single.list()
+	
+Donations
+---------
+We accept PayPal through:
+
+https://www.paypal.me/netkiller
+
+Wechat (微信) / Alipay (支付宝) 打赏:
+
+http://www.netkiller.cn/home/donations.html
--- shell/struts2.sh
+++ shell/struts2.sh
@@ -4,7 +4,7 @@
 # Author: neo <netkiller@msn.com>  
 ########################################
 # http://struts.apache.org/docs/s2-046.html
-# Struts S2-046, S2-045 Â©¶´·À»ðÇ½
+# Struts S2-046, S2-045 Firewall
 ########################################
 keywords=( "bash" "cmd" "struts" "ognl" "os.name" "win" "opensymphony" "redirectErrorStream" "ProcessBuilder" "getOutputStream" "getInputStream" "start()" "copy(" "flush()")
 for keyword in "${keywords[@]}"
